57ab1cf551aeff45437116c2d85cc7cc5a82951a
GilangPambudi <2131710126@student.polinema.ac.id>
2024-04-25 14:50:47 +0700

    Edited col.

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -9,8 +9,8 @@
         <form method="POST" action="{{ url('user') }}" class="form-horizontal">
         @csrf
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Role</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">Role</label>
+            <div class="col-10">
                 <select class="form-control" id="id_role" name="id_role" required>
                     <option value="">- Pilih Role -</option>
                 @foreach($role as $item)
@@ -23,8 +23,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Nama</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">Nama</label>
+            <div class="col-10">
                 <input type="text" class="form-control" id="nama" name="nama" value="{{ old('nama') }}" required>
             @error('nama')
                 <small class="form-text text-danger">{{ $message }}</small>
@@ -32,8 +32,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Email</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">Email</label>
+            <div class="col-10">
                 <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}" required>
             @error('email')
                 <small class="form-text text-danger">{{ $message }}</small>
@@ -41,8 +41,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">No HP</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">No HP</label>
+            <div class="col-10">
                 <input type="text" class="form-control" id="no_hp" name="no_hp" value="{{ old('no_hp') }}" required>
             @error('no_hp')
                 <small class="form-text text-danger">{{ $message }}</small>
@@ -50,8 +50,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Username</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">Username</label>
+            <div class="col-10">
                 <input type="text" class="form-control" id="username" name="username" value="{{ old('username') }}" required>
             @error('username')
                 <small class="form-text text-danger">{{ $message }}</small>
@@ -59,8 +59,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label">Password</label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label">Password</label>
+            <div class="col-10">
                 <input type="password" class="form-control" id="password" name="password" required>
             @error('password')
                 <small class="form-text text-danger">{{ $message }}</small>
@@ -68,8 +68,8 @@
             </div>
         </div>
         <div class="form-group row">
-            <label class="col-1 control-label col-form-label"></label>
-            <div class="col-11">
+            <label class="col-2 control-label col-form-label"></label>
+            <div class="col-10">
                 <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
                 <a class="btn btn-sm btn-default ml-1" href="{{ url('user') }}">Kembali</a>
             </div>
